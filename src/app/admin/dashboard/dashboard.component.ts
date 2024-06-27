02165c1a5645dd50e9454473906fcd51efdba86b
Sonawane Kranti Prakash (BD/TOA-ETS1) <Kranti.PrakashSonawane@bosch.com>
2024-06-27 17:46:18 +0530

    home page

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports:[CommonModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
-})
-export class DashboardComponent {
+  styleUrls: ['./dashboard.component.scss'],
 
+
+})
+export class DashboardComponent implements OnInit {
+
+  toggleProperty = false;
+
+  constructor() { }
+  ngOnInit(): void {
+    // Initialization code here
+  }
+
+  toggle() {
+    this.toggleProperty = !this.toggleProperty;
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './admin/home/home.component';
 import { LabCommissionComponent } from './components/lab_commission/lab_commission.component';
 import { SelfCheckComponent } from './components/self-check/self-check.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path:'',
     component:HomeComponent,
     children:[
+      {path:'dashboard', component:DashboardComponent},
       {path:'lab_commission', component:LabCommissionComponent},
       {path:'self-check', component: SelfCheckComponent}
     ]
