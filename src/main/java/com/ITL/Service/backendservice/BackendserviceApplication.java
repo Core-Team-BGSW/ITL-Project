b3d44c7d4435d7605d24f9121eec5d5da340627f
Core-Team-BGSW <mohit.somvanshi@bosch.com>
2024-07-09 14:18:56 +0530

    Csv to Database but not finalizd

--- src/main/java/com/ITL/Service/backendservice/BackendserviceApplication.java
+++ src/main/java/com/ITL/Service/backendservice/BackendserviceApplication.java
@@ -3,9 +3,11 @@ package com.ITL.Service.backendservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableScheduling
 public class BackendserviceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ITL/Service/backendservice/Controller/ExcelToCsvController.java
+++ src/main/java/com/ITL/Service/backendservice/Controller/ExcelToCsvController.java
@@ -2,14 +2,6 @@ package com.ITL.Service.backendservice.Controller;
 
 import com.ITL.Service.backendservice.Service.ExcelToCsvService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,24 +14,10 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class ExcelToCsvController {
     private final ExcelToCsvService excelToCsvService;
-    private final JobLauncher jobLauncher;
-    private final Job job;
 
     @PostMapping("/convert-excel-to-csv")
     public ResponseEntity<String> convert(@RequestParam("file") MultipartFile file) {
         return excelToCsvService.excelToCsvConverter(file);
-    }
-    @PostMapping("/csv_data")
-    public void importCsvToDBJob() {
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("Start At: ", System.currentTimeMillis())
-                .toJobParameters();
-        try {
-            jobLauncher.run(job, jobParameters);
-        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException |
-                 JobParametersInvalidException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
 
