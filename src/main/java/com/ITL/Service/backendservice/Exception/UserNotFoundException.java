9b08905b40f34e6ef08fa8c8f9e9c302836167bb
Core-Team-BGSW <mohit.somvanshi@bosch.com>
2024-07-28 16:16:14 +0530

    temparory formData is mapped but need more functionality

--- src/main/java/com/ITL/Service/backendservice/Exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ITL.Service.backendservice.Exception;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
