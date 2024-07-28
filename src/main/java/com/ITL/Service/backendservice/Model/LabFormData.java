8ae6f0f30446a32b53b12d0328cacfc752e04207
Core-Team-BGSW <mohit.somvanshi@bosch.com>
2024-07-28 16:16:14 +0530

    temparory formData is mapped but need more functionality

---
+++ src/main/java/com/ITL/Service/backendservice/Model/LabFormData.java
@@ -0,0 +1,38 @@
+package com.ITL.Service.backendservice.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LabFormData {
+    private String region;
+    private String country;
+    private String location;
+    private String locationCode;
+    private String gb;
+    private String local_itl;
+    private String entityName;
+    private String local_itl_proxy;
+    private String dh;
+    private String kam;
+    private String dep_name;
+    private String building;
+    private String floor;
+    private String labNo;
+    private String primary_lab_cord;
+    private String secondary_lab_cord;
+    private String cost_center;
+    private String kind_of_lab;
+    private String purpose_of_lab;
+    private String description;
+    private String new_equipment;
+    private String shared_lab;
+    private String acl_req;
+    private String green_ports;
+    private String yellow_ports;
+    private String red_ports;
+}
