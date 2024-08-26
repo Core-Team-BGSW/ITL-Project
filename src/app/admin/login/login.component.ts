02eeeb56f16bca37e5a71c565c1f0a78a3267072
Jay-Jambhale <jaynandkumar.jambhale@in.bosch.com>
2024-08-26 16:48:58 +0530

    Dialogboxupdated lab commission

--- src/app/admin/login/login.component.ts
+++ src/app/admin/login/login.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  // username: string = '';
+  // password: string = '';
+
+  // constructor(private router: Router) {}
+
+  // onLogin() {
+  //   // For demo purposes, use static credentials.
+  //   // Replace with actual authentication logic.
+  //   if (this.username === 'admin' && this.password === 'password') {
+  //     localStorage.setItem('authToken', 'some-token'); // Store auth token or similar
+  //     this.router.navigate(['/home']);
+  //   } else {
+  //     alert('Invalid credentials!');
+  //   }
+  // }
 
 }
