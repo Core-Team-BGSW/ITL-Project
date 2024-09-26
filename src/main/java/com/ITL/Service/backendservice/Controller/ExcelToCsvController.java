f99fdaadf47f6d375805cf1935c555fb9249ee9b
Somvanshi Mohit (BD/TOA-ETS1) <obv3kor@bosch.com>
2024-09-26 17:32:42 +0530

    some changes in Excel to Csv Service file

--- src/main/java/com/ITL/Service/backendservice/Controller/ExcelToCsvController.java
+++ src/main/java/com/ITL/Service/backendservice/Controller/ExcelToCsvController.java
@@ -17,8 +17,8 @@ public class ExcelToCsvController {
 
     @PostMapping("/convert-excel-to-csv")
     public ResponseEntity<String> convert(@RequestParam("file") MultipartFile file) {
-        System.out.println("HI..");
         return excelToCsvService.excelToCsvConverter(file);
     }
 }
+
 
