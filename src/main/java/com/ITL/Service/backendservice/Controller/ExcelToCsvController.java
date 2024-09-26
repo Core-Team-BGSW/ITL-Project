064c0c0ed62f4d18930630614a47186bece07200
Kranti.PrakashSonawane@bosch.com <Kranti.PrakashSonawane@bosch.com>
2024-09-26 17:37:00 +0530

    checking

--- src/main/java/com/ITL/Service/backendservice/Controller/ExcelToCsvController.java
+++ src/main/java/com/ITL/Service/backendservice/Controller/ExcelToCsvController.java
@@ -17,7 +17,7 @@ public class ExcelToCsvController {
 
     @PostMapping("/convert-excel-to-csv")
     public ResponseEntity<String> convert(@RequestParam("file") MultipartFile file) {
-        System.out.println("HI..");
+
         return excelToCsvService.excelToCsvConverter(file);
     }
 }
