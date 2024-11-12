e276200c09471449778ce8dd949a1c387ea08d03
Kranti.PrakashSonawane@bosch.com <Kranti.PrakashSonawane@bosch.com>
2024-11-13 00:10:10 +0530

    changed role form

--- src/app/admin/contact/contact.component.ts
+++ src/app/admin/contact/contact.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
-
+  selectedCategory: string = '';
 
 }
