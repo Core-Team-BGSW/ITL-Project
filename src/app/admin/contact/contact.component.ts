926e75d0720e70c31caf854b3ed533549226566b
Ashraf2301 <AshrafRamjan.Shaikh@in.bosch.com>
2024-12-10 16:28:45 +0530

    contact form design updates

--- src/app/admin/contact/contact.component.ts
+++ src/app/admin/contact/contact.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../service/login.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  selectedCategory: string = '';
+  username = '';
+  ntid: any = '';
+  department:any="";
+
+  constructor(private loginService: LoginService){}
+  ngOnInit(): void {
+      this.getuserid();
+  }
+
+
+  getuserid() {
+    this.username = this.loginService.getUserId();
+    this.ntid = (this.loginService.getNtId())?.toUpperCase();
+    this.department=this.username.match(/\(([^)]+)\)/)?.[1];
+  }
 
 
 
 }
+
+
